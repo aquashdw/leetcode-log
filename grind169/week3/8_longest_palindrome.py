2d60d78392eecf8a4a312675bd292439d4e44d05
aquashdw <aquashdw@gmail.com>
2025-01-12 22:11:02 +0900

    longest-palindrome https://leetcode.com/problems/longest-palindrome/

---
+++ grind169/week3/8_longest_palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counts = {}
+        for char in s:
+            counts[char] = counts.get(char, 0) + 1
+
+        usable_letters = 0
+        odd = False
+        for char in counts:
+            count = counts[char]
+            usable_letters += count
+            if count % 2 != 0:
+                odd = True
+                usable_letters -= 1
+        return usable_letters + (1 if odd else 0)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.longestPalindrome(
+        "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
