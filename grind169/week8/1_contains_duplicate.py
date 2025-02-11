5714c1a2ac88863c3c8777c7ca4ac1448720cee8
aquashdw <aquashdw@gmail.com>
2025-02-11 16:07:13 +0900

    contains-duplicate https://leetcode.com/problems/contains-duplicate/

---
+++ grind169/week8/1_contains_duplicate.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        contains = set()
+        for num in nums:
+            if num in contains:
+                return True
+            contains.add(num)
+        return False
