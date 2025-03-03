db23f41383c5248954cdf7cb5a4857a9ceb7b74d
aquashdw <aquashdw@gmail.com>
2025-03-03 22:39:25 +0900

    (need study) find-the-duplicate-number https://leetcode.com/problems/find-the-duplicate-number/

---
+++ grind169/week11/1_find_the_duplicate_num.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        fast = nums[nums[nums[0]]]
+        slow = nums[nums[0]]
+        while fast != slow:
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+
+        fast = nums[0]
+        while fast != slow:
+            fast = nums[fast]
+            slow = nums[slow]
+
+        return slow
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.findDuplicate([3, 1, 2, 4, 2]))
